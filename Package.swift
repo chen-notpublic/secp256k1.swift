e340133364fa724ffa4015f8a78c938483f0490a
chen-notpublic <97544311+chen-notpublic@users.noreply.github.com>
2022-08-26 09:53:45 +0800

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -7,7 +7,7 @@ let package = Package(
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
-            name: "secp256k1",
+            name: "secp256k1swift",
             targets: ["secp256k1swift"]),
     ],
     dependencies: [
